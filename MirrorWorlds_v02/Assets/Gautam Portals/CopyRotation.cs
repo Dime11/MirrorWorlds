6962e97a99f0c9ea41892773b37250149dfabc54
Dime11 <57994863+Dime11@users.noreply.github.com>
2022-03-25 15:24:04 +0200

    Frankenstaining the rotation code of the portal IT WORKS

--- MirrorWorlds_v02/Assets/Gautam Portals/CopyRotation.cs
+++ MirrorWorlds_v02/Assets/Gautam Portals/CopyRotation.cs
@@ -8,6 +8,11 @@ public class CopyRotation : MonoBehaviour
     public float rotationFactorY = 0.5f;
     public float rotationFactorZ = 0.5f;
     public Transform target;
+
+    public float juliaNumX;
+    public float juliaNumY;
+    public float juliaNumZ;
+
     
     float currentAngleX;
     float currentAngleY;
@@ -19,13 +24,13 @@ public class CopyRotation : MonoBehaviour
         SetNegativeAngles (out currentAngleY, target.localEulerAngles.y);
         SetNegativeAngles (out currentAngleZ, target.localEulerAngles.z);
 
-        transform.localEulerAngles = new Vector3(currentAngleX * rotationFactorX, currentAngleY * rotationFactorY, currentAngleZ * rotationFactorZ);
+        transform.localEulerAngles = new Vector3(currentAngleX * rotationFactorX +juliaNumX, currentAngleY * rotationFactorY+juliaNumY, currentAngleZ * rotationFactorZ+juliaNumZ);
     }
     
     float SetNegativeAngles(out float angle, float refFloat)
     { 
         angle = refFloat;
-        angle = (angle > 180) ? angle - 360 : angle;
+        angle = (angle > 270) ? angle - 360 : angle;
         return angle;
     }
 
--- MirrorWorlds_v02/Assets/Gautam Portals/CopyRotation2.cs
+++ MirrorWorlds_v02/Assets/Gautam Portals/CopyRotation2.cs
@@ -15,6 +15,7 @@ public class CopyRotation2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = target.transform.rotation;
+        //transform.rotation = target.transform.rotation;
+        //transform.rotation = new Vector3(target.transform.rotation.x *0.1f, target.transform.rotation.y *0.1f, target.transform.rotation.z *0.1f);
     }
 }
